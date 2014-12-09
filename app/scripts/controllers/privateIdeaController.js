a8a49bca1b0fe7285563212b9e70b65adbcf8ead
Ludvig Stenström <ludste@kth.se>
2014-12-09 12:23:56 +0100

    Added confirmation on successful delete

--- app/scripts/controllers/privateIdeaController.js
+++ app/scripts/controllers/privateIdeaController.js
@@ -10,38 +10,7 @@ angular.module('uiApp')
   .controller('MainController', ['$scope', '$log', 'Ideas', function ($scope, $log, Ideas) {
     $scope.init = function () {
       $scope.ideas = Ideas.getPrivateIdeas();
-      /*    $scope.ideas = [{
-       id: 32,
-       name: 'Idea that someone want to buy',
-       description: 'This is a cool idea that I want to buy. The Idea is about a team that is really good at coding and want to sell themselves for very little money.',
-       buyer: null,
-       published: false
-
-       }, {
-       id: 1,
-       name: 'Idea that someone want to buy',
-       description: 'This is a cool idea that I want to buy. The Idea is about a team that is really good at coding and want to sell themselves for very little money.',
-       buyer: null,
-       published: true
-
-       },
-       {
-       id: 54,
-       name: 'Idea that someone want to buy',
-       description: 'This is a cool idea that I want to buy. ' +
-       'asdas ' +
-       ' das' +
-       'das dasdas asd das das ds n fbsda, badsl gkdThe Idea is about a team that is really good at coding and want to sell themselves for very little money.',
-       buyer: {
-       first_name: 'Kalle',
-       last_name: 'Svensson',
-       email: 'mymail@mail.com',
-       phone: '070666-666'
-       },
-       published: false
-       }
-       ];*/
-    };
+    }
     $scope.respondToBuyRequest = function (idea, index, respose) {
       if (respose) {
         idea.accepted = true;
@@ -61,6 +30,8 @@ angular.module('uiApp')
     $scope.deleteIdea = function (idea, index) {
       $('#deleteIdea').modal('toggle');
       Ideas.delete({ideaId: idea.id}, function (response) {
+        $log.debug(response);
+        $scope.message = "Your idea have been deleted!";
       });
       $scope.ideas.splice(index, 1);
     };
@@ -76,4 +47,6 @@ angular.module('uiApp')
       });
     };
     $scope.init();
-  }]);
+  }
+  ])
+;
